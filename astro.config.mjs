9b01551676860be66131f6f9f290d3e346e66b88
Fiber <leaf@airo.cc>
2025-10-16 20:11:37 +0800

    fix cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'static',
-  adapter: cloudflare({
-    mode: 'directory',
-    imageService: 'compile',
-  }),
+  // output: 'static',
+  // adapter: cloudflare({
+  //   mode: 'directory',
+  //   imageService: 'compile',
+  // }),
   integrations: [
     react(),
     tailwind({
